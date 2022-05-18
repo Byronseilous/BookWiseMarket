4e3c45aefab4dc4d51c769cd298c42403fb6c3fe
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2022-05-19 02:02:15 +0300

    feat: Implement JavaScript functionality (index.js)

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const config = require('./config/dev');
+const FakeDB = require('./fake.db')
+const Rental = require('./models/rental')
+
+const rentalRoutes = require('./routes/rentals')
+const userRoutes = require('./routes/users')
+const bookingRoutes = require('./routes/bookings')
+
+
+mongoose.connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  const fakeDB = new FakeDB
+  // fakeDB.seedDb()
+})
+
+const app = express()
+app.use(bodyParser.json())
+
+
+app.use('/api/v1/rentals', rentalRoutes)
+app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/bookings', bookingRoutes)
+
+
+
+app.get('/rentals', function (req, res) {
+  res.json({ 'success': true })
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function () {
+  console.log('I am running!')
+})
+
