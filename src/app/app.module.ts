815762353a3c88c25f72c0be2d34527ea75de656
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2022-11-04 01:22:35 +0300

    chore: Update app.module.ts (app.module.ts)

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+
+import { RentalModule } from './rental/rental.module';
+import { AuthModule } from './auth/auth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AuthComponent } from './auth/auth.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ManageModule } from './manage/manage.module';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'rentals', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule,
+    AuthModule,
+    NgbModule.forRoot(),
+    ToastModule.forRoot(),
+    BrowserAnimationsModule,
+    ManageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
